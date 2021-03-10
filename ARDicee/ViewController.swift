2b8d7a608a75f7c2a627754252a9df37382bbe55
Bogdan Orzea <bogdan.orzea@gmail.com>
2021-03-09 21:44:54 -0500

    Adds 3d moon

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,15 +19,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
 
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let sphere = SCNSphere(radius: 0.2)
 
         let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        cube.materials = [material]
+        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+        sphere.materials = [material]
 
         let node = SCNNode()
         node.position = SCNVector3(0, 0.1, -0.5)
-        node.geometry = cube
+        node.geometry = sphere
 
         sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
